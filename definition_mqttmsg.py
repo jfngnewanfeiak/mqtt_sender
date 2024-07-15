6acad370e0904de4cd6c2d1d3303d725912b403e
OK220259\taishin <ok220259@oks.iput.ac.jp>
2024-07-15 21:49:01 +0900

    aaa

--- definition_mqttmsg.py
+++ definition_mqttmsg.py
@@ -4,4 +4,8 @@ class RobotActionMsg:
     ReqRobot1 = 'Req/Robot/1'
     ReqRobot2 = 'Req/Robot/2'
 
-# ロボットからの情報
+# ロボットからの位置情報を貰うためのメッセージ
+class GetRobotPosition:
+    ReqRobotPosition0 = 'Req/Robot/Position/0'
+    ReqRobotPosition1 = 'Req/Robot/Position/1'
+    ReqRobotPosition2 = 'Req/Robot/Position/2'
---
+++ robot_position_name.py
@@ -0,0 +1,8 @@
+
+class RobotPositionName:
+    warehouse0 = 'warehouse0'
+    warehouse1 = 'warehouse1'
+    warehouse2 = 'warehouse2'
+    waitposition0 = 'waitposition0'
+    waitposition1 = 'waitposition1'
+    waitposition2 = 'waitposition2'
